fbfe67dfaa96e913d245a59454a0826d01086bcc
Iupac120 <oforokpara@gmail.com>
2022-05-19 14:36:16 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+console.log('starting server')
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.status(200).sendFile(path.join(__dirname,'views','index.html'))
+    
+})
+app.get('/about',(req,res)=>{
+    res.status(200).sendFile(path.join(__dirname,'views','about.html'))
+    
+})
+app.get('/contact-me',(req,res)=>{
+    res.status(200).sendFile(path.join(__dirname,'views','contact-me.html'))
+   
+})
+app.get('/*',(req,res)=>{
+    res.status(500).sendFile(path.join(__dirname,'views','404.html'))
+
+})
+
+
+app.listen(8080,()=>{
+    console.log('server is starting')
+})
